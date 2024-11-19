20caaec771def859c677e7c21eaced149ae27852
Brian <brianhammer777@yahoo.com>
2024-11-19 12:42:38 -0500

    first commit

---
+++ lib/hello_app_web/controllers/page_controller.ex
@@ -0,0 +1,19 @@
+defmodule HelloAppWeb.PageController do
+  use HelloAppWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+
+  def hello(conn, _params) do
+    html(conn, "hello world!")
+  end
+
+  def goodbye(conn, _params), do: html(conn, "Goodbye idiot!")
+
+  def chao(conn, _params) do
+    html(conn, "¡Chao! World")
+  end
+end
